9ee125c541d50e3364fa52a0db817980cbe0c8af
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-07-22 14:51:41 +0200

    finish style for homepage and install react-router

--- portolio-front/src/components/Aside.jsx
+++ portolio-front/src/components/Aside.jsx
@@ -2,5 +2,13 @@ import React from "react";
 import "../styles/Aside.scss";
 
 export default function Aside() {
-  return <div className="asideMenu"></div>;
+  return (
+    <div className="asideMenu">
+      <img className="avatar" src="/avatar.png" alt="mon avatar" />
+      <img src="/house.png" alt="accueil" />
+      <img src="/contact.png" alt="contactez-moi" />
+      <img src="/project.png" alt="my work" />
+      <img src="/skill.png" alt="my skills" />
+    </div>
+  );
 }
