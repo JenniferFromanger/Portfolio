a7749ffd2af233eecefc2263b7eb2ba3b0ba1e67
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-07-24 14:19:25 +0200

    add useReducer on aside

--- portolio-front/src/components/Aside.jsx
+++ portolio-front/src/components/Aside.jsx
@@ -1,22 +1,91 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/Aside.scss";
 
 export default function Aside() {
+  const initialState = {
+    textHomepage: false,
+    textContact: false,
+    textProject: false,
+    textSkill: false,
+  };
+
+  function reducer(state = { ...initialState }, action) {
+    switch (action.type) {
+      case "setTextHomepage":
+        return { textHomepage: (state.textHomepage = true) };
+      case "setImageHomepage":
+        return { textHomepage: (state.textHomepage = false) };
+      case "setTextContact":
+        return { textContact: (state.textContact = true) };
+      case "setImageContact":
+        return { textContact: (state.textContact = false) };
+      case "setTextProject":
+        return { textProject: (state.textProject = true) };
+      case "setImageProject":
+        return { textProject: (state.textProject = false) };
+      case "setTextSkill":
+        return { textSkill: (state.textSkill = true) };
+      case "setImageSkill":
+        return { textProject: (state.textSkill = false) };
+      default:
+        throw new Error();
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="asideMenu">
-      <img className="avatar" src="/avatar.png" alt="mon avatar" />
-      <NavLink exact to="/">
-        <img className="picto" src="/house.png" alt="accueil" />
+      <img className="avatar" src="/avatar.png" alt="my avatar" />
+      <NavLink
+        exact
+        to="/"
+        onMouseEnter={() => dispatch({ type: "setTextHomepage" })}
+        onMouseLeave={() => dispatch({ type: "setImageHomepage" })}
+      >
+        {!state.textHomepage ? (
+          <img className="picto" src="/house.png" alt="homepage" />
+        ) : (
+          <p>Homepage</p>
+        )}
       </NavLink>
-      <NavLink to="/contact">
-        <img className="picto" src="/contact.png" alt="contactez-moi" />
+      <NavLink
+        to="/contact"
+        onMouseEnter={() => dispatch({ type: "setTextContact" })}
+        onMouseLeave={() => dispatch({ type: "setImageContact" })}
+      >
+        {!state.textContact ? (
+          <img className="picto" src="/contact.png" alt="contact-me" />
+        ) : (
+          <p>Contact-me</p>
+        )}
       </NavLink>
-      <NavLink to="/projects">
-        <img className="picto" src="/project.png" alt="my work" />
+      <NavLink
+        to="/projects"
+        onMouseEnter={() => dispatch({ type: "setTextProject" })}
+        onMouseLeave={() => dispatch({ type: "setTextProject" })}
+      >
+        {!state.textProject ? (
+          <img className="picto" src="/project.png" alt="my work" />
+        ) : (
+          <p>My work</p>
+        )}
       </NavLink>
-      <img className="picto" src="/skill.png" alt="my skills" />
-      <img className="picto" src="/log-in.png" alt="my skills" />
+      <NavLink
+        to="/Skills"
+        onMouseEnter={() => dispatch({ type: "setTextSkill" })}
+        onMouseLeave={() => dispatch({ type: "setImageSkill" })}
+      >
+        {!state.textSkill ? (
+          <img className="picto" src="/skill.png" alt="my skills" />
+        ) : (
+          <p>My skills</p>
+        )}
+      </NavLink>
+      <NavLink to="/Private">
+        <img className="picto" src="/log-in.png" alt="private " />
+      </NavLink>
     </div>
   );
 }
