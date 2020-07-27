d38054daae0c960c67e4c559241a63475b5edf32
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-07-27 18:08:05 +0200

    add first step with my messages

--- portolio-front/src/components/Dashboard.jsx
+++ portolio-front/src/components/Dashboard.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { backend } from "../conf";
 import "../styles/Dashboard.scss";
 
 export default function Dashboard() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${backend}/contacts`).then(({ data }) => {
+      setContacts(data);
+    });
+  }, []);
+
   return (
     <div className="dash">
       <h2>My Dashboard</h2>
+      <p>My messages</p>
+      {contacts.map((contact) => {
+        return (
+          <div className="messages">
+            <p>{contact.date}</p>
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+            <p>{contact.subject}</p>
+            <p>{contact.content}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
