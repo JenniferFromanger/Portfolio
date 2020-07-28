fdd47d29453750b3b6c2c34a7450998d236b9c3f
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-07-28 08:47:24 +0200

    add css on list of messages

--- portolio-front/src/components/Dashboard.jsx
+++ portolio-front/src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { backend } from "../conf";
+import { Grid } from "@material-ui/core";
 import "../styles/Dashboard.scss";
 
 export default function Dashboard() {
@@ -15,18 +16,35 @@ export default function Dashboard() {
   return (
     <div className="dash">
       <h2>My Dashboard</h2>
-      <p>My messages</p>
-      {contacts.map((contact) => {
-        return (
-          <div className="messages">
-            <p>{contact.date}</p>
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
-            <p>{contact.subject}</p>
-            <p>{contact.content}</p>
-          </div>
-        );
-      })}
+      <Grid item xs={6}>
+        <h3>My messages</h3>
+        {contacts.map((contact) => {
+          return (
+            <div className="messages">
+              <p>
+                <span>Sending date : </span>
+                {contact.date}
+              </p>
+              <p>
+                <span>Contact Name : </span>
+                {contact.name}
+              </p>
+              <p>
+                <span>Contact Email : </span>
+                {contact.email}
+              </p>
+              <p>
+                <span>Subject : </span>
+                {contact.subject}
+              </p>
+              <p>
+                <span>Message : </span>
+                {contact.content}
+              </p>
+            </div>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
